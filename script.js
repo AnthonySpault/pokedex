0fc2b0d343627a37eca44ef0d44b2a0315e565e4
Anthony Spault <anthony@spault.fr>
2017-02-10 17:04:25 +0100

    New features

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 window.onload = function() {
   var http = new XMLHttpRequest();
   var url = "pokemons.json";
-  var picture = document.getElementById("picture");
+  var picture = document.querySelector("#picture");
+  var contentBox = document.querySelector("#stats");
+  var notFound = "http://ecole.spault.fr/pokedex/notfound.jpg";
+  var baseUrlPokemon = "http://img.pokemondb.net/artwork/";
   http.open("GET", url, true);
   http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   http.onload = function() {
@@ -9,44 +12,53 @@ window.onload = function() {
       var array = JSON.parse(this.responseText);
       Pokedex(array);
     }
-    else {
-      var errors = JSON.parse(http.responseText);
-      for (var error in errors['errors']){
-      console.log(error+' : '+errors['errors'][error]);
-    }
-  }
-};
-
-function Pokedex(array) {
-  document.forms['pokemon'].onsubmit = function(){
-    var regex = new RegExp('^[0-9]+$');
-    var input = this.elements['input'].value.toLowerCase();
-    if (!regex.test(input)) {
-      for (var key in array) {
-        if (input === array[key.toString()].name.toLowerCase()) {
-          var content = array[key.toString()].name + "<br>Type : " + array[key.toString()].type;
-          picture.style.backgroundImage = 'url("http://img.pokemondb.net/artwork/' + array[key.toString()].name.toLowerCase() + '.jpg")';
+  };
+  function Pokedex(array) {
+    document.forms['pokemon'].onsubmit = function(){
+      var regex = new RegExp('^[0-9]+$');
+      var input = this.elements['input'].value.toLowerCase();
+      if (!regex.test(input)) {
+        for (var key in array) {
+          if (input === array[key].name.toLowerCase()) {
+            var cleanPokemon = array[key].name.toLowerCase().replace(". ","-").replace("'","");
+            if (input == "nidoran") {
+              if (confirm("Ok for male, Cancel for female.")) {
+                cleanPokemon = "nidoran-m";
+              }
+              else {
+                cleanPokemon = "nidoran-f";
+              }
+            }
+            var content = array[key.toString()].name + "<br>Type : " + array[key.toString()].type;
+            picture.style.backgroundImage = 'url("' + baseUrlPokemon + cleanPokemon + '.jpg")';
+            break;
+          }
+          else {
+            var content = "Pokemon " + input + " not found";
+            picture.style.backgroundImage = 'url("' + notFound + '")';
+          }
         }
-        else {
-          var content = "Pokemon " + input + " not found";
-          picture.style.backgroundImage = 'url("http://vignette2.wikia.nocookie.net/assassinscreed/images/3/39/Not-found.jpg")';
-        }
-      }
-    }
-    else {
-      if (input >= 1 && input <= 151) {
-        var content = array[input].name + "<br>Type : " + array[input].type;
-        picture.style.backgroundImage = 'url("http://img.pokemondb.net/artwork/' + array[input].name.toLowerCase() + '.jpg")';
       }
       else {
-        var content = "Pokemon number " + input + " not found";
-        picture.style.backgroundImage = 'url("http://vignette2.wikia.nocookie.net/assassinscreed/images/3/39/Not-found.jpg")';
+        if (input >= 1 && input <= 151) {
+          var cleanPokemon = array[input].name.toLowerCase().replace(". ","-").replace("'","");
+          if (input == 29) {
+            cleanPokemon = "nidoran-f";
+          }
+          if (input == 32) {
+            cleanPokemon = "nidoran-m";
+          }
+          var content = array[input].name + "<br>Type : " + array[input].type;
+          picture.style.backgroundImage = 'url("' + baseUrlPokemon + cleanPokemon + '.jpg")';
+        }
+        else {
+          var content = "Pokemon number " + input + " not found";
+          picture.style.backgroundImage = 'url("' + notFound + '")';
+        }
+      }
+      contentBox.innerHTML = content;
+      return false;
       }
     }
-    document.getElementById("stats").innerHTML = content;
-    return false;
-    }
-  }
-  http.send();
-  return false;
+    http.send();
 };
